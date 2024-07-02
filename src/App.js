1ea7b8c72ae52330f9f9efe950b555b0cd730e1a
satyam <satyamkale271@gmail.com>
2024-07-02 16:11:54 +0530

    friend owe add

--- src/App.js
+++ src/App.js
@@ -44,9 +44,17 @@ function Friend({ friend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {friend.balance}{" "}
+          You owe {friend.name} {Math.abs(friend.balance)}$
         </p>
       )}
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      <h3>{friend.name}</h3>
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
     </li>
   );
 }
