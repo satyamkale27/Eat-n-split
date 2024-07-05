97e71b095b483997e787a6d49ff46891ade93321
satyam <satyamkale271@gmail.com>
2024-07-05 20:00:39 +0530

    in FormSplitBill x replaced with friend name came from prop

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ export default function App() {
           {showAddFriend ? "close" : "Add friend"}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill />}
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
     </div>
   );
 }
@@ -127,25 +127,25 @@ function FormAddFriend({ onaddfriends }) {
     </form>
   );
 }
-function FormSplitBill() {
+function FormSplitBill({ selectedFriend }) {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
       <label>💰 Bill value</label>
 
       <input type="text" />
       <label>🧍‍♀️ your expense</label>
 
       <input type="text" />
-      <label>👫 x's expense</label>
+      <label>👫 {selectedFriend.name} expense</label>
 
       <input type="text" disabled />
       <label>🤑 who is paying the bill</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
-      <Button>Split bi ll</Button>
+      <Button>Split bill</Button>
     </form>
   );
 }
